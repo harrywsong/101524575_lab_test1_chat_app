3262b6d98700686503536e1ea81ab15c207e988f
Woohyuk Song <harrywsong@naver.com>
2026-02-11 17:55:35 -0500

    Added better validation for user on backend

--- models/User.js
+++ models/User.js
@@ -4,24 +4,29 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    required: [true, 'Username is required'],
     unique: true,
-    trim: true
+    trim: true,
+    minlength: [3, 'Username must be at least 3 characters long'],
+    maxlength: [30, 'Username must be less than 30 characters']
   },
   firstname: {
     type: String,
-    required: true,
-    trim: true
+    required: [true, 'First name is required'],
+    trim: true,
+    maxlength: [50, 'First name must be less than 50 characters']
   },
   lastname: {
     type: String,
-    required: true,
-    trim: true
+    required: [true, 'Last name is required'],
+    trim: true,
+    maxlength: [50, 'Last name must be less than 50 characters']
   },
   password: {
     type: String,
-    required: true,
-    minLength: 6
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters long'],
+    maxlength: [100, 'Password must be less than 100 characters']
   },
   createon: {
     type: Date,
